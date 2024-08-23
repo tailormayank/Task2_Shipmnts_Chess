44ba6021d0f28d1aa33ff8c35636a067843c9ea6
mayank_tailor <tailormayank6953@gamil.com>
2024-08-23 22:37:23 +0530

    Returns possible moves

--- index.js
+++ index.js
@@ -119,6 +119,102 @@ $(document).ready(function () {
     setColor();
     setTheme();
   });
+
+  $(function () {
+
+   
+
+    //Pawn promotion event
+    $("#pawn-promotion-option .option").on("click", function () {
+      var newType = $(this).attr("id");
+      promotion.box.html(chessPieces[promotion.color][newType]);
+      promotion.box.addClass("placed");
+      promotion.box.attr("piece", promotion.color + "-" + newType);
+
+      $("#pawn-promotion-option").addClass("hide");
+      $("#game").css("opacity", "1");
+
+      promotion = {};
+    });
+
+    //Reset game
+    $("#restart-btn").on("click", function () {
+      resetGame();
+    });
+
+    //Restart when game over
+    $("#result").on("click", function () {
+      resetGame();
+    });
+
+    //Box click event
+    $(".box").on("click", function () {
+      if ($(this).hasClass("selected")) {
+        //Undo to select new box
+        $(this).removeClass("selected");
+
+        $(".box").removeClass("suggest");
+        select = { canMove: false, piece: "", box: "" };
+        return;
+      }
+
+      //Select new box
+      if (!select.canMove) {
+        //Check the right color to play
+        if ($(this).attr("piece").indexOf(player) >= 0) {
+          //Select a piece to move
+          selectPiece($(this));
+        }
+      }
+
+      //Set up new destination for selected box
+      else if (select.canMove) {
+        var selectedPieceInfo = select.piece.split("-");
+        var color = selectedPieceInfo[0];
+        var type = selectedPieceInfo[1];
+
+        //Select new piece to move if 2 colors are the same
+        if ($(this).attr("piece").indexOf(color) >= 0) {
+          $("#" + select.box).removeClass("selected");
+          $(".box").removeClass("suggest");
+          //Select a piece to move
+          selectPiece($(this));
+          return;
+        }
+
+        //Can move if it is valid
+        if ($(this).hasClass("suggest")) {
+          //Save move in history
+          var move = {
+            previous: {},
+            current: {},
+          };
+
+          move.previous.piece = select.piece;
+          move.previous.box = select.box;
+
+          move.current.piece = $(this).attr("piece");
+          move.current.box = $(this).attr("id");
+
+          historyMoves.push(move);
+
+          //Move selected piece successfully
+          setPiece($(this), color, type);
+
+          //Delete moved box
+          deleteBox($("#" + select.box));
+
+          $(".box").removeClass("suggest");
+
+          select = { canMove: false, piece: "", box: "" };
+
+          //Switch player
+          switchPlayer();
+        }
+      }
+    });
+  });
+
   var setNewBoard = function (box, i, j) {
     if (j === 7) {
       if (i === 0 || i === 7) {
@@ -154,56 +250,53 @@ $(document).ready(function () {
       }
     }
   };
-    //Switch player
-	var switchPlayer = function() {
-        if(player === 'black') {
-             $('#player').html(chessPieces.white.king);
-             player = 'white';
-        } else {
-             $('#player').html(chessPieces.black.king);
-             player = 'black';
-        }
-   }
+  //Switch player
+  var switchPlayer = function () {
+    if (player === "black") {
+      $("#player").html(chessPieces.white.king);
+      player = "white";
+    } else {
+      $("#player").html(chessPieces.black.king);
+      player = "black";
+    }
+  };
 
-   var setPiece = function(box, color, type) {
-
+  var setPiece = function (box, color, type) {
     //Check end game (if king is defeated)
-    if(box.attr('piece').indexOf('king') >= 0) {
-         showWinner(player);
+    if (box.attr("piece").indexOf("king") >= 0) {
+      showWinner(player);
 
-         box.html(chessPieces[color][type]);
-         box.addClass('placed');
-         box.attr('piece', color + '-' + type);
+      box.html(chessPieces[color][type]);
+      box.addClass("placed");
+      box.attr("piece", color + "-" + type);
 
-         return;
+      return;
     }
 
     //Check if pawn reached the last line
-    var j = parseInt(box.attr('id').charAt(6));
-    if(type === 'pawn') {
-         if( (player === 'black' && j === 7) ||
-               (player === 'white' && j === 0)) {
-               $('#game').css('opacity', '0.5');
+    var j = parseInt(box.attr("id").charAt(6));
+    if (type === "pawn") {
+      if ((player === "black" && j === 7) || (player === "white" && j === 0)) {
+        $("#game").css("opacity", "0.5");
 
-               var option = $('#pawn-promotion-option');
-               option.removeClass('hide');
-               option.find('#queen').html(chessPieces[player].queen);
-               option.find('#rook').html(chessPieces[player].rook);
-               option.find('#knight').html(chessPieces[player].knight);
-               option.find('#bishop').html(chessPieces[player].bishop);
+        var option = $("#pawn-promotion-option");
+        option.removeClass("hide");
+        option.find("#queen").html(chessPieces[player].queen);
+        option.find("#rook").html(chessPieces[player].rook);
+        option.find("#knight").html(chessPieces[player].knight);
+        option.find("#bishop").html(chessPieces[player].bishop);
 
-               promotion = { box: box, color: color };
+        promotion = { box: box, color: color };
 
-               return;
-         }
+        return;
+      }
     }
 
     box.html(chessPieces[color][type]);
-    box.addClass('placed');
-    box.attr('piece', color + '-' + type);
-    if (type === 'archer') {
-       archerMoveType = (archerMoveType === 'bishop') ? 'rook' : 'bishop';
-   }
-}
-
+    box.addClass("placed");
+    box.attr("piece", color + "-" + type);
+    if (type === "archer") {
+      archerMoveType = archerMoveType === "bishop" ? "rook" : "bishop";
+    }
+  };
 });
