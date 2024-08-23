6cd4c70f65dd8873fcd1f7fd642d9b59648020fc
mayank_tailor <tailormayank6953@gamil.com>
2024-08-23 22:16:11 +0530

    Chess pieces added

---
+++ index.js
@@ -0,0 +1,88 @@
+	//Chess pieces
+	var chessPieces = {
+        'white': {
+             'king': '&#9812;',
+             'queen': '&#9813;',
+             'rook': '&#9814;',
+             'bishop': '&#9815;',
+             'archer':'&#x262e;',
+             'knight': '&#9816;',
+             'pawn': '&#9817;'
+        },
+        'black': {
+             'king': '&#9818;',
+             'queen': '&#9819;',
+             'rook': '&#9820;',
+             'bishop': '&#9821;',
+             'knight': '&#9822;',
+             'pawn': '&#9823;'
+        }
+   };
+
+   var player = 'white'; //First player
+
+angular.module('myApp', []).controller('GameController', ['$scope', function($scope) {
+	
+	//Draw the board game
+    $scope.size = 8;
+    $scope.widths = [];
+
+    //Draw board
+    for(var i = 0; i < $scope.size; i++) { 
+        $scope.widths.push(i);
+    }
+	
+}]);
+
+
+var colorOption = 0;
+var themeOption = 1;
+var archerMoveType = 'bishop'; 
+//Change theme
+$('#theme-option').on('click', function() {
+    themeOption === themes.length - 1 ? themeOption = 0 : themeOption++;
+    
+    setTheme();
+});
+
+//Set up theme
+var setTheme = function() {
+    var theme = themes[themeOption];
+    
+    $('#theme-option').html(theme.name);
+    
+    $('#board').css('border-color', theme.boardBorderColor);
+    $('.light-box').css('background', theme.lightBoxColor);
+    $('.dark-box').css('background', theme.darkBoxColor);
+    
+    $('.option-nav').css('color', theme.optionColor);
+    
+    //Option Menu effect
+    $('.button').css('color', theme.optionColor);
+    $('.button').hover(
+        function() {
+            $(this).css('color', theme.optionHoverColor);
+        }, function() {
+            $(this).css('color', theme.optionColor);
+        }
+    );
+}
+
+//Change color
+$('#color-option').on('click', function() {
+   colorOption === colors.length - 1 ? colorOption = 0 : colorOption++;
+    
+    setColor();
+});
+
+var setColor = function() {
+    var color = colors[colorOption];
+    
+    $('#color-option').html(color.name);
+    
+    $('.box').css('color', color['color']);
+    
+    $('#pawn-promotion-option').css('color', color['color']);
+    
+    $('#player').css('color', color['color']);
+}
